15ae077d4d9d53d2653b455b1f99ba6972665233
Md Mostafizur Rahman <mostafizur1102@gmail.com>
2024-08-14 21:27:38 +0600

    before render

--- user_profile/views.py
+++ user_profile/views.py
@@ -63,11 +63,11 @@ def activate(request, uid64, token):
     if user is not None and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save()
-        return redirect('login')
-        # return redirect('https://mygardenhub.netlify.app/login/')
+        # return redirect('login')
+        return redirect('https://goodyhub.netlify.app/login/')
     else:
-        return redirect('register')
-        # return redirect('https://mygardenhub.netlify.app/register/')
+        # return redirect('register')
+        return redirect('https://goodyhub.netlify.app/register/')
     
 
 class UserLoginApiView(APIView):
